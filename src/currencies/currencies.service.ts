7d506123eb726735475521b2086b5827f0abe3d1
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-19 16:55:19 -0300

    ensure createCurrency() pass over the tests

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,8 +1,20 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
+export class CurrenciesEntity {
+  currency: string;
+  value: string;
+}
 export class CurrenciesRepository {
-  async getCurrency(currency: string): Promise<any> {
-    //
+  async getCurrency(currency: string): Promise<CurrenciesEntity> {
+    return new CurrenciesEntity();
+  }
+
+  async createCurrency({ currency, value }): Promise<CurrenciesEntity> {
+    return new CurrenciesEntity();
   }
 }
 
@@ -10,11 +22,15 @@ export class CurrenciesRepository {
 export class CurrenciesService {
   constructor(private currenciesRepository: CurrenciesRepository) {}
 
-  async getCurrency(currency: string): Promise<any> {
-    try {
-      return await this.currenciesRepository.getCurrency(currency);
-    } catch (error) {
-      throw new InternalServerErrorException();
+  async getCurrency(currency: string): Promise<CurrenciesEntity> {
+    return await this.currenciesRepository.getCurrency(currency);
+  }
+
+  async createCurrency({ currency, value }): Promise<CurrenciesEntity> {
+    if (value <= 0) {
+      throw new BadRequestException('The value must be grater than zero.');
     }
+
+    return await this.currenciesRepository.createCurrency({ currency, value });
   }
 }

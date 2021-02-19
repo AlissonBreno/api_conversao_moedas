60d9a4692b650e7b24d70972e0adc24f1ad6e1f8
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-19 10:44:42 -0300

    ensure  covnertAmount called getCurrency twice

--- src/exchange/tests/exchange.service.spec.ts
+++ src/exchange/tests/exchange.service.spec.ts
@@ -1,16 +1,25 @@
 import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from '../exchange.service';
+import { CurrenciesService, ExchangeService } from '../exchange.service';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
+    const currenciesServiceMock = {
+      getCurrency: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService,
+        { provide: CurrenciesService, useFactory: () => currenciesServiceMock },
+      ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -31,6 +40,12 @@ describe('ExchangeService', () => {
         await expect(
           service.covnertAmount({ from: 'USD', to: 'BRL', amount: 1 })
         ).resolves.not.toThrow(new BadRequestException());
+      });
+    });
+    describe('getCurrency() should be called', () => {
+      it('twice', async () => {
+        await service.covnertAmount({ from: 'USD', to: 'BRL', amount: 1 });
+        await expect(currenciesService.getCurrency).toBeCalledTimes(2);
       });
     });
   });

0ec1d30e63d5783520aa4896355ecb7cc08a36e8
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-22 08:58:50 -0300

    update currencies.entity

--- src/currencies/entities/currencies.entity.ts
+++ src/currencies/entities/currencies.entity.ts
@@ -1,4 +1,29 @@
+import { timeStamp } from 'console';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ObjectIdColumn,
+  PrimaryColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Unique(['currency'])
+@Entity()
 export class CurrenciesEntity {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
   currency: string;
+
+  @Column()
   value: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  UpdateAt: Date;
 }

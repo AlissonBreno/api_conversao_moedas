f90774604d2b56a313c0d47ae6389f6bef2633c2
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-22 09:59:51 -0300

    refactoring currencies entity

--- src/currencies/entities/currencies.entity.ts
+++ src/currencies/entities/currencies.entity.ts
@@ -1,4 +1,5 @@
-import { timeStamp } from 'console';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, Length } from 'class-validator';
 import {
   Column,
   CreateDateColumn,
@@ -16,9 +17,14 @@ export class CurrenciesEntity {
   _id: string;
 
   @PrimaryColumn()
+  @Length(3, 3)
+  @IsNotEmpty()
   currency: string;
 
   @Column()
+  @IsNotEmpty()
+  @IsNumber()
+  @Type(() => Number)
   value: number;
 
   @CreateDateColumn({ type: 'timestamp' })
